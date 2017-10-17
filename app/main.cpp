4865c7985237331cfe49c93fd5bba6d7b7499c91
Jessica H <jesmhoward@gmail.com>
2017-10-17 00:13:53 -0400

    RRT-26 Added demo

--- app/main.cpp
+++ app/main.cpp
@@ -3,18 +3,38 @@
 
 int main()
 {
-  int startX = 0;
-  int startY = 0;
-  int goalX = 12;
-  int goalY = 12;
-  int step = 5;
-  int radius = 5;
-  Obstacle obs(15, 15, 3);
-  std::list<Obstacle> obsList;
+  //Set our starting variables
+  int start_x = 0;          //Path beginning x location
+  int start_y = 0;          //Path beginning y location
+  int goal_x = 12;           //Goal x location
+  int goal_y = 12;           //Goal y location
+  int step = 5;             //Size of step
+  int radius = 5;           //How close do we need to be to the goal?
+  int map_width = 15;       //Width of map
+  int map_height = 15;      //Height of map
 
-  Map specificMap(15, 15, obsList);
-  specificMap.addObstacle(obs);
-  RRTPath rrt(specificMap, startX, startY, goalX, goalY, step, radius);
-  std::list<std::pair<int, int>> path = rrt.findPath();
-    return 0;
+  //Create an empty list for our obstacles
+  std::list<Obstacle> obstacle_list;
+
+  //Create a map of size map_height, map_width
+  Map specificMap(map_height, map_width, obstacle_list);
+
+  //Create an obstacle (you can create more)
+  Obstacle obs(15, 15, 3);
+  //Add our obstacle to our map
+  specificMap.add_obstacle(obs);
+
+  //Create our path object
+  RRTPath rrt(specificMap, start_x, start_y, goal_x, goal_y, step, radius);
+
+  //Start it running and save the path
+  std::list<std::pair<int, int>> path = rrt.find_path();
+
+  //Print the path for funsies so we can see where we went
+  std::list<std::pair<int, int>>::iterator it;
+  std::cout << "Path from start to goal" << std::endl;
+  for (it = path.begin(); it != path.end(); ++it) {
+    std::cout << it->first << ", " << it->second << std::endl;
+  }
+  return 0;
 }
